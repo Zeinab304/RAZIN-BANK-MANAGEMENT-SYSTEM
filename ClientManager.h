b1e4e05eff394d61b5c09745132d4304a69b674c
Zeinab-Ayman <zeinabayman304@gmail.com>
2026-05-08 17:41:31 +0300

    Update ClientManager.h

--- ClientManager.h
+++ ClientManager.h
@@ -14,71 +14,92 @@ class ClientManager
 		cout << "------------------------------------\n";
 		cout << "          Client Main Menu          \n";
 		cout << "------------------------------------\n";
-		cout << "\n [1] WithDraw\n";
-		cout << "\n [2] Deposit\n";
-		cout << "\n [3] Check Balance\n";
-		cout << "\n [4] Display My Info\n";
-		cout << "\n [5] Update Passward\n";
-		cout << "\n [6] Logout\n";
-		cout << "\n [?] Pleace enter a choice (1-6) : ";
+		cout << "\n [1] WithDraw" << endl;
+		cout << "\n [2] Deposit" << endl;
+		cout << "\n [3] Transfer To" << endl;
+		cout << "\n [4] Check Balance" << endl;
+		cout << "\n [5] Display My Info" << endl;
+		cout << "\n [6] Update Passward" << endl;
+		cout << "\n [7] Logout" << endl;
+		cout << "\n [?] Pleace enter a choice (1-7) : ";
 	}
+
 	static void updatePassword(Person* person) {
+		string newpass;
+		new_page;
 		cout << "---------------------------------\n";
 		cout << "          UPDATE PASSWARD        \n";
 		cout << "---------------------------------\n";
-		string newpass;
-		new_page;
 		cout << " Enter New Password: ";
 		cin >> newpass;
 		Validation::checkPWD(newpass);
+
 		person->setPassword(newpass);
-		FilesHelper::updateClientsFile();
 		cout << "\n [^-^] Password Updated Successfully\n";
 		cout << "\n\n\n\n";
 		system("pause");
 
 	}
-public:
-	
-
-	static Client* login(int id, string password) {
-		for (int i = 0; i < AllClients.size(); i++) {
-			if (AllClients[i].getId() == id && AllClients[i].getPassword() == password) {
-				cout << "[*] Login successful! Welcome, " << AllClients[i].getName();
-				return &AllClients[i];
-			}
-		}
-		return nullptr;
-	}
-
 
 	static void withdrawOperation(Client* client) {
-		new_page;
 		double amount;
+		new_page;
 		cout << "-------------------------------------\n";
 		cout << "         WITHDRAW OPERATION          \n";
 		cout << "-------------------------------------\n";
 		cout << "\n [~] Please enter amount to withdraw: ";
 		cin >> amount;
 		client->withdraw(amount);
+		FilesHelper::updateClientsFile();
 		time_op;
 		cout << "\n\n\n\n";
 		system("pause");
-
 	}
+
 	static void depositOperation(Client* client) {
-		new_page;
 		double amount;
+		new_page;
 		cout << "-------------------------------------\n";
 		cout << "         DEPOSIT OPERATION          \n";
 		cout << "-------------------------------------\n";
 		cout << "\n [~] Please enter amount to deposit: ";
 		cin >> amount;
 		client->deposit(amount);
+		FilesHelper::updateClientsFile();
 		time_op;
 		cout << "\n\n\n\n";
 		system("pause");
 	}
+
+	static void transferOperation(Client* client) {
+		double amount;
+		new_page;
+		cout << "-------------------------------------\n";
+		cout << "         TRANSFER OPERATION         \n";
+		cout << "-------------------------------------\n";
+		cout << "\n [~] Please enter recipient ID: ";
+		int recipientId;
+		cin >> recipientId;
+		Client* recipient = nullptr;
+		for (int i = 0; i < AllClients.size(); i++) {
+			if (AllClients[i].getId() == recipientId) {
+				recipient = &AllClients[i];
+				break;
+			}
+		}
+		if (recipient) {
+			cout << " [~] Please enter amount to transfer: ";
+			cin >> amount;
+			client->transferTo(amount, *recipient);
+			FilesHelper::updateClientsFile();
+			time_op;
+		}
+		else
+			cout << "\n [!] Recipient not found!";
+		cout << "\n\n\n\n";
+		system("pause");
+	}
+
 	static void checkBalance(Client* client) {
 		new_page;
 		cout << "---------------------------------\n";
@@ -88,6 +109,7 @@ public:
 		cout << "\n\n\n\n";
 		system("pause");
 	}
+
 	static void clientInfo(Client* client) {
 		new_page;
 		cout << "-----------------------------\n";
@@ -98,36 +120,50 @@ public:
 		system("pause");
 	}
 
+public:
+
+	static Client* login(int id, string password) {
+		for (int i = 0; i < AllClients.size(); i++) {
+			if (AllClients[i].getId() == id && AllClients[i].getPassword() == password) {
+				cout << "\n [*] Login successful! Welcome, " << AllClients[i].getName();
+				cout << "\n\n\n\n";
+				system("pause");
+				return &AllClients[i];
+			}
+		}
+		return nullptr;
+	}
 
 	static bool clientOptions(Client* client) {
 		printClientMenu();
 
 		int choice;
-		double amount;
 
 		cin >> choice;
-		switch(choice){
+		switch (choice) {
 		case 1:
 			withdrawOperation(client);
 			break;
 		case 2:
 			depositOperation(client);
 			break;
-			
 		case 3:
-			checkBalance( client);
+
+			transferOperation(client);
 			break;
 		case 4:
-			clientInfo(client);
+			checkBalance(client);
 			break;
 		case 5:
+			clientInfo(client);
+			break;
+		case 6:
 			updatePassword(client);
 			FilesHelper::updateClientsFile();
 			break;
-		case 6:
+		case 7:
 			new_page;
 			cout << "\n [*] Thanks " << client->getName() << " for your time ,Goodbye (*^_^*)\n\n\n";
-			system("pause");
 			return false;
 			break;
 		default:
@@ -135,5 +171,6 @@ public:
 		}
 		return true;
 	}
+
 };
 
